eef562652a614aca46bf7d0c84fc5f176bd03a2f
Grzegorz Cagara <grzegorzcagara@gmail.com>
2025-10-29 22:08:22 +0100

    *adding demo functionality *using more bootstrap components *using styled components

--- src/tabs/morningRoutine/MorningRoutine.tsx
+++ src/tabs/morningRoutine/MorningRoutine.tsx
@@ -1,20 +1,34 @@
 import React, {CSSProperties, useEffect, useState} from 'react';
 import {Volume2, VolumeOff} from "lucide-react";
 import notificationSound from '../../assets/sounds/notification.mp3';
+import {Button, Col, Container, ListGroup, Row} from "react-bootstrap";
+import styled from "styled-components";
 
 type Stage = {
     name: string;
     durationInSeconds: number;
 };
 
-const stages: Stage[] = [
-    { name: '⏰ 🥱 Wstawanie - 15 minut', durationInSeconds: 15*60 },
-    { name: '👚 👗 Ubieranie się - 10 minut', durationInSeconds: 10*60 },
-    { name: '🍽️ 🥣 Śniadanie - 20 minut', durationInSeconds: 20*60 },
-    { name: '𖥈 🪥🦷 Czesanie i mycie zębów - 5 minut', durationInSeconds: 5*60 },
-    { name: '👟 🧥 Ubieranie butów i kurtek - 5 minut', durationInSeconds: 5*60 },
-    { name: '🚶🏻 🚗💨 Wychodzenie z mieszkania - 5 minut', durationInSeconds: 5*60 },
+interface RoutineStartTime {
+    hour: number;
+    minute: number;
+    second: number;
+}
+
+const initialStages: Stage[] = [
+    {name: '⏰ 🥱 Wstawanie - 15 minut', durationInSeconds: 15 * 60},
+    {name: '👚 👗 Ubieranie się - 10 minut', durationInSeconds: 10 * 60},
+    {name: '🍽️ 🥣 Śniadanie - 20 minut', durationInSeconds: 20 * 60},
+    {name: '𖥈 🪥🦷 Czesanie i mycie zębów - 5 minut', durationInSeconds: 5 * 60},
+    {name: '👟 🧥 Ubieranie butów i kurtek - 5 minut', durationInSeconds: 5 * 60},
+    {name: '🚶🏻 🚗💨 Wychodzenie z mieszkania - 5 minut', durationInSeconds: 5 * 60},
 ];
+
+const initialRoutineStartTime: RoutineStartTime = {
+    hour: 6,
+    minute: 30,
+    second: 0,
+};
 
 // const stages: Stage[] = [
 //     { name: '⏰ 🥱 Wstawanie - 5 minut', durationInSeconds: 5*60 },
@@ -27,23 +41,21 @@ const stages: Stage[] = [
 // ];
 export const MorningRoutine: React.FC = () => {
     // morning routine stages
+    const [stages, setStages] = useState<Stage[]>(initialStages)
     const [currentStageIndex, setCurrentStageIndex] = useState(9);
     const [elapsedTime, setElapsedTime] = useState(0);
     const [isBeforeRoutine, setIsBeforeRoutine] = useState(false);
     const [isRoutineActive, setIsRoutineActive] = useState(false);
     const [isAfterRoutine, setIsAfterRoutine] = useState(false);
-    const routineStartTime= {
-        hour: 6,
-        minute: 30,
-        second: 0,
-    };
+    const [routineStartTime, setRoutineStartTime] = useState<RoutineStartTime>(initialRoutineStartTime)
+
     const [isStageTimeCalculated, setIsStageTimeCalculated] = useState(false);
     const [timeLeft, setTimeLeft] = useState("00:00:00");
     const todayTargetTime = new Date().setHours(routineStartTime.hour, routineStartTime.minute, routineStartTime.second);
     const totalStageTime = stages.reduce((acc, stage) => acc + stage.durationInSeconds, 0);
 
+    const [demoMode, setDemoMode] = useState<boolean>(false);
     // audio
-    const [buttonColor, setButtonColor] = useState<string>('#4CAF50');
     const [audioMuted, setAudioMuted] = useState<boolean>(false);
     const [audio] = useState(() => {
         const audioElement = new Audio(notificationSound);
@@ -74,9 +86,9 @@ export const MorningRoutine: React.FC = () => {
             const now = new Date().getTime();
             let remaining = target - now;
 
-            if (remaining > 0 ) {
+            if (remaining > 0) {
                 setIsBeforeRoutine(true);
-            } else if(remaining <= 0 && (remaining*(-1) <= (totalStageTime+2) * 1000)){
+            } else if (remaining <= 0 && (remaining * (-1) <= (totalStageTime + 2) * 1000)) {
                 setIsBeforeRoutine(false);
                 setIsRoutineActive(true);
                 setTimeLeft("00:00:00");
@@ -86,8 +98,8 @@ export const MorningRoutine: React.FC = () => {
                 remaining = now - targetPlusStages;
             }
 
-            if (isRoutineActive && !isStageTimeCalculated){
-                calculateStageTime(Math.floor(Math.abs(remaining)/1000));
+            if (isRoutineActive && !isStageTimeCalculated) {
+                calculateStageTime(Math.floor(Math.abs(remaining) / 1000));
             } else if (isRoutineActive && isStageTimeCalculated) {
                 setElapsedTime((prev) => {
                     if (currentStageIndex >= stages.length) return prev;
@@ -101,7 +113,7 @@ export const MorningRoutine: React.FC = () => {
                 });
             }
 
-            if(isAfterRoutine){
+            if (isAfterRoutine) {
                 remaining = now - targetPlusStages;
             }
 
@@ -122,7 +134,7 @@ export const MorningRoutine: React.FC = () => {
 
     //start stage in proper state after reload
     useEffect(() => {
-        if (isRoutineActive){
+        if (isRoutineActive) {
             setCurrentStageIndex(0);
         }
     }, [isRoutineActive]);
@@ -137,78 +149,80 @@ export const MorningRoutine: React.FC = () => {
 
     const toggleSoundMute = () => {
         if (audioMuted) {
-            setButtonColor('#4CAF50');
             setAudioMuted(false);
         } else {
-            setButtonColor('#e1a1a1');
             setAudioMuted(true);
             audio.pause();
             audio.load();
         }
     };
 
-    const overlayStyle: CSSProperties = {
-        position: "absolute",
-        inset: "0px",
-        backgroundColor: "rgb(136 136 136 / 59%)",
-        zIndex: "10",
-        borderRadius: "8px",
-        fontWeight: "600",
-        fontSize: "2.5rem",
-        fontFamily: "monospace"
+    const runDemoMode = () => {
+        setStages([
+            {name: '⏰ 🥱 Wstawanie - 5 sekund - DEMO', durationInSeconds: 5},
+            {name: '👚 👗 Ubieranie się - 5 sekund - DEMO', durationInSeconds: 5},
+            {name: '🍽️ 🥣 Śniadanie - 5 sekund - DEMO', durationInSeconds: 5},
+            {name: '𖥈 🪥🦷 Czesanie i mycie zębów - 5 sekund - DEMO', durationInSeconds: 5},
+            {name: '👟 🧥 Ubieranie butów i kurtek - 5 sekund - DEMO', durationInSeconds: 5},
+            {name: '🚶🏻 🚗💨 Wychodzenie z mieszkania - 5 sekund - DEMO', durationInSeconds: 5},
+        ]);
+        const demoStartTime = new Date();
+        const demoStartHour = demoStartTime.getHours();
+        const demoStartMinute = demoStartTime.getMinutes();
+        const demoStartSecond = demoStartTime.getSeconds()
+        setRoutineStartTime({hour: demoStartHour, minute: demoStartMinute, second: demoStartSecond} as RoutineStartTime)
+        setIsBeforeRoutine(false);
+        setIsAfterRoutine(false);
+        setIsRoutineActive(true);
+        setDemoMode(true);
+    }
+
+    const initiateStandardRoutine = () => {
+        setStages(initialStages);
+        setRoutineStartTime(initialRoutineStartTime)
+        setCurrentStageIndex(9)
+        setDemoMode(false);
     }
 
     return (
-        <div style={{fontFamily: 'Arial, sans-serif'}}>
-                <div style={{marginBottom: "10px"}}>
-                    <button
-                        type="button"
-                        onClick={toggleSoundMute}
-                        style={{
-                            padding: '10px 20px',
-                            fontSize: '16px',
-                            backgroundColor: buttonColor,
-                            color: 'white',
-                            border: 'none',
-                            borderRadius: '5px',
-                            cursor: 'pointer',
-                        }}
-                    >
-                        {audioMuted ? <VolumeOff color="white"/> : <Volume2 color="white"/>}
-                    </button>
-                </div>
+        <Container style={{padding: 0, fontFamily: 'Arial, sans-serif'}}>
+            <div className="flex-row-reverse mb-2" style={{textAlign: 'right'}}>
+                <Button
+                    onClick={toggleSoundMute}
+                    style={{
+                        backgroundColor: audioMuted ? '#e1a1a1' : '#4CAF50',
+                        border: `1px solid ${audioMuted ? '#e1a1a1' : '#4CAF50'}`,
+                        marginRight: '10px',
+                    }}
+                >
+                    {audioMuted ? <VolumeOff/> : <Volume2/>}
+                </Button>
+                <Button
+                    onClick={demoMode ? initiateStandardRoutine : runDemoMode}
+                > <strong>{demoMode ? 'RESET' : 'DEMO'}</strong> </Button>
+            </div>
 
-                <div className="position-relative">
-                    {isBeforeRoutine &&
-                        <div className="text-white p-3" style={overlayStyle}>
-                            <div style={{
-                                height: "100%",
-                                display: "flex",
-                                justifyContent: "center",
-                                alignItems: "center"
-                            }}>
-                                Rutyna rozpocznie się za
-                                <br/>
-                                {timeLeft}
-                            </div>
-                        </div>}
 
-                    {isAfterRoutine &&
-                        <div className="text-white p-3" style={overlayStyle}>
-                            <div style={{
-                                height: "100%",
-                                display: "flex",
-                                justifyContent: "center",
-                                alignItems: "center"
-                            }}>
-                                Jest już po rutynie:
-                            <br/>
-                                {timeLeft}
-                            </div>
-                        </div>}
-                <ul style={{listStyleType: 'none', padding: 0}}>
+            <Row className="position-relative m-0">
+                {isBeforeRoutine && <OverlayContainer>
+                    <OverlayContent>
+                        {demoMode && "TRYB DEMO AKTYWNY! ⏱️🚀"} <br/>
+                        Rutyna rozpocznie się za <br/> {timeLeft}
+                    </OverlayContent>
+                </OverlayContainer>
+                }
+
+                {isAfterRoutine && <OverlayContainer>
+                    <OverlayContent>
+                        {demoMode && <>"TRYB DEMO AKTYWNY! ⏱️🚀" <br/></>}
+                        Jest już po rutynie: <br/> {timeLeft}
+                    </OverlayContent>
+                </OverlayContainer>}
+
+                <ListGroup style={{listStyleType: 'none', padding: 0}}>
                     {stages.map((stage, index) => {
                         const isActive = index === currentStageIndex;
+                        const isLastStage = index === stages.length - 1;
                         let progress = 0;
 
                         if (isActive && stage.durationInSeconds > 0) {
@@ -220,49 +234,73 @@ export const MorningRoutine: React.FC = () => {
                         }
 
                         return (
-                            <li
-                                key={index}
-                                style={{
-                                    padding: '0.5rem 1rem',
-                                    marginBottom: '1rem',
-                                    backgroundColor: isActive ? '#4caf50' : '#e0e0e0',
-                                    color: isActive ? '#fff' : '#000',
-                                    fontWeight: isActive ? 'bold' : 'normal',
-                                    borderRadius: '8px',
-                                    transition: 'background-color 0.5s ease',
-                                    boxShadow: isActive ? '0 0 10px rgba(0,0,0,0.3)' : 'none',
-                                }}
-                            >
-                                <div
-                                    style={{fontSize: '1.2rem', marginBottom: '0.5rem', textAlign: 'left'}}>{stage.name}
+                            <ListGroup.Item key={index} style={listGroupItemStyle(isActive, isLastStage)}>
+                                <Col
+                                    style={{fontSize: '1.2rem', marginBottom: '0.5rem', textAlign: 'left'}}>
+                                    {stage.name}
                                     {isActive && (
                                         <div style={{marginBottom: '0.5rem', textAlign: 'end', float: 'right'}}>
                                             Pozostało: {getRemainingTime(stage.durationInSeconds, elapsedTime)}
                                         </div>
                                     )}
-                                </div>
-                                <div
-                                    style={{
-                                        height: '10px',
-                                        backgroundColor: '#ccc',
-                                        borderRadius: '5px',
-                                        overflow: 'hidden',
-                                    }}
-                                >
-                                    <div
-                                        style={{
-                                            width: `${progress}%`,
-                                            height: '100%',
-                                            backgroundColor: isActive ? '#fff' : '#888',
-                                            transition: 'width 1s linear',
-                                        }}
-                                    />
-                                </div>
-                            </li>
+                                </Col>
+                                <ProgressBarContainer>
+                                    <ProgressBarFiller progress={progress} isActive={isActive}/>
+                                </ProgressBarContainer>
+                            </ListGroup.Item>
                         );
                     })}
-                </ul>
-            </div>
-        </div>
+                </ListGroup>
+            </Row>
+        </Container>
     );
 };
+
+const listGroupItemStyle = (isActive: boolean, isLastStage: boolean): CSSProperties => {
+    return {
+        padding: isActive ? '1rem 1rem' : '0.5rem 1rem',
+        marginBottom: isLastStage ? 0 : '1rem',
+        border: "none",
+        backgroundColor: isActive ? '#4caf50' : '#e0e0e0',
+        color: isActive ? '#fff' : '#000',
+        fontWeight: isActive ? 'bold' : 'normal',
+        borderRadius: '8px',
+        transition: 'background-color 1s ease',
+        boxShadow: isActive ? '0 0 10px rgba(0,0,0,0.3)' : 'none',
+        display: 'grid',
+    }
+}
+
+const OverlayContainer = styled(Col)`
+    position: absolute;
+    inset: 0px;
+    background-color: rgba(136, 136, 136, 0.59);
+    z-index: 10;
+    border-radius: 8px;
+    font-weight: 600;
+    font-size: 2.5rem;
+    font-family: monospace;
+    color: white;
+`;
+
+const OverlayContent = styled(Col)`
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ProgressBarContainer = styled.div`
+    height: 10px;
+    background-color: #ccc;
+    border-radius: 5px;
+    overflow: hidden;
+`;
+
+const ProgressBarFiller = styled.div<{ progress: number; isActive: boolean }>`
+    width: ${props => props.progress}%;
+    height: 100%;
+    background-color: ${props => (props.isActive ? '#fff' : '#888')};
+    transition: width 1s linear;
+`;
+
