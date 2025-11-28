54fee03e0e251bc3c7412ddcd0aa32bb26ccc98f
Grzegorz Cagara <grzegorzcagara@gmail.com>
2025-11-28 18:06:56 +0100

    *using better variable names *formatting

--- src/tabs/trams/TramSchedule.tsx
+++ src/tabs/trams/TramSchedule.tsx
@@ -64,7 +64,7 @@ const TramSchedule: React.FC = () => {
         setSchedule(tramScheduleData);
         calculateNextDeparture(tramScheduleData);
 
-        const current = tramScheduleData.timetables.find(t => t.isInEffect);
+        const current = tramScheduleData.timetables.find(timetable => timetable.isInEffect);
         if (current) setActiveDay(current.dayName);
 
         setLoading(false);
@@ -114,89 +114,90 @@ const TramSchedule: React.FC = () => {
     };
 
     return (
-            <Card>
-                {/*<Card.Header>*/}
-                {/*    Rozkład jazdy tramwaju*/}
-                {/*    <Button variant="outline-primary" size="sm" className="float-end" onClick={fetchTramScheduleDetails}>*/}
-                {/*        Odśwież*/}
-                {/*    </Button>*/}
-                {/*</Card.Header>*/}
-                <Card.Body>
-                    {loading ? (
-                        <Spinner animation="border" role="status">
-                            <span className="visually-hidden">Ładowanie...</span>
-                        </Spinner>
-                    ) : schedule ? (
-                        <>
-                            <Card.Title style={{textAlign: "center"}}>Linia: 20 - Przystanek: Mały Płaszów (id: {schedule.departureId.stopId})
-                            </Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">
-                                Operator: {schedule.carrierDescription.namePl}
-                            </Card.Subtitle>
+        <Card>
+            {/*<Card.Header>*/}
+            {/*    Rozkład jazdy tramwaju*/}
+            {/*    <Button variant="outline-primary" size="sm" className="float-end" onClick={fetchTramScheduleDetails}>*/}
+            {/*        Odśwież*/}
+            {/*    </Button>*/}
+            {/*</Card.Header>*/}
+            <Card.Body>
+                {loading ? (
+                    <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Ładowanie...</span>
+                    </Spinner>
+                ) : schedule ? (
+                    <>
+                        <Card.Title style={{textAlign: "center"}}>Linia: 20 - Przystanek: Mały Płaszów
+                            (id: {schedule.departureId.stopId})
+                        </Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">
+                            Operator: {schedule.carrierDescription.namePl}
+                        </Card.Subtitle>
 
-                            {nextDepartureTime && (
-                                <Alert variant="info">
-                                    <strong>Najbliższy
-                                        odjazd:</strong> {nextDepartureTime.toLocaleTimeString()} (za {countdown})
+                        {nextDepartureTime && (
+                            <Alert variant="info">
+                                <strong>Najbliższy
+                                    odjazd:</strong> {nextDepartureTime.toLocaleTimeString()} (za {countdown})
+                            </Alert>
+                        )}
+
+                        {schedule.timetables
+                            .filter(timetable => timetable.isInEffect)
+                            .map((timetable, index) => (
+                                <Alert key={index} variant="success" className="mb-3">
+                                    <strong>Obowiązujący rozkład:</strong> {timetable.dayName}
                                 </Alert>
-                            )}
+                            ))}
 
-                            {schedule.timetables
-                                .filter(t => t.isInEffect)
-                                .map((t, index) => (
-                                    <Alert key={index} variant="success" className="mb-3">
-                                        <strong>Obowiązujący rozkład:</strong> {t.dayName}
-                                    </Alert>
-                                ))}
+                        <Tabs
+                            activeKey={activeDay ?? ''}
+                            onSelect={(key) => {
+                                setActiveDay(key ?? '');
+                                if (schedule) calculateNextDeparture(schedule);
+                            }}
+                            className="mb-3"
+                        >
+                            {schedule.timetables.map((timetable) => (
+                                <Tab eventKey={timetable.dayName} title={timetable.dayName} key={timetable.dayName}>
 
-                            <Tabs
-                                activeKey={activeDay ?? ''}
-                                onSelect={(k) => {
-                                    setActiveDay(k ?? '');
-                                    if (schedule) calculateNextDeparture(schedule);
-                                }}
-                                className="mb-3"
-                            >
-                                {schedule.timetables.map((timetable) => (
-                                    <Tab eventKey={timetable.dayName} title={timetable.dayName} key={timetable.dayName}>
+                                    <ListGroup>
+                                        {timetable.rows.map((row) => (
+                                            <ListGroup.Item
+                                                key={`${timetable.dayName}-${row.hour}`}
+                                                className={timetable.isInEffect && row.minutes.some(min => `${row.hour}-${min}` === highlightedRowKey) ? 'highlight-row' : ''}
+                                            >
+                                                <Row>
+                                                    <Col xs={3} sm={2}>
+                                                        <strong>{row.hour.toString().padStart(2, '0')}</strong>
+                                                    </Col>
+                                                    <Col xs={9} sm={10} className="text-start d-flex flex-wrap">
+                                                        {row.minutes.map((minuteStr, index) => {
+                                                            const now = new Date();
+                                                            const hour = row.hour;
+                                                            const minute = parseInt(minuteStr, 10);
+                                                            const departureTime = new Date();
+                                                            departureTime.setHours(hour, minute, 0, 0);
+                                                            const diffMs = departureTime.getTime() - now.getTime();
 
-                                        <ListGroup>
-                                            {timetable.rows.map((row) => (
-                                                <ListGroup.Item
-                                                    key={`${timetable.dayName}-${row.hour}`}
-                                                    className={timetable.isInEffect && row.minutes.some(min => `${row.hour}-${min}` === highlightedRowKey) ? 'highlight-row' : ''}
-                                                >
-                                                    <Row>
-                                                        <Col xs={3} sm={2}>
-                                                            <strong>{row.hour.toString().padStart(2, '0')}</strong>
-                                                        </Col>
-                                                        <Col xs={9} sm={10} className="text-start d-flex flex-wrap">
-                                                            {row.minutes.map((minuteStr, index) => {
-                                                                const now = new Date();
-                                                                const hour = row.hour;
-                                                                const minute = parseInt(minuteStr, 10);
-                                                                const departureTime = new Date();
-                                                                departureTime.setHours(hour, minute, 0, 0);
-                                                                const diffMs = departureTime.getTime() - now.getTime();
-
-                                                                let color = 'inherit';
-                                                                let pulseClass = '';
-                                                                if (timetable.isInEffect){
-                                                                    if (`${hour}-${minute}` === `${nextDepartureTime?.getHours()}-${nextDepartureTime?.getMinutes()}`) {
-                                                                        color = 'green';
-                                                                        pulseClass = 'pulse';
-                                                                    }
+                                                            let color = 'inherit';
+                                                            let pulseClass = '';
+                                                            if (timetable.isInEffect) {
+                                                                if (`${hour}-${minute}` === `${nextDepartureTime?.getHours()}-${nextDepartureTime?.getMinutes()}`) {
+                                                                    color = 'green';
+                                                                    pulseClass = 'pulse';
                                                                 }
+                                                            }
 
-                                                                const formattedTime = `${hour.toString().padStart(2, '0')}:${minuteStr}`;
+                                                            const formattedTime = `${hour.toString().padStart(2, '0')}:${minuteStr}`;
 
-                                                                return (
-                                                                    <OverlayTrigger
-                                                                        key={index}
-                                                                        placement="top"
-                                                                        overlay={<Tooltip id={`tooltip-${index}`}>Odjazd
-                                                                            o {formattedTime}</Tooltip>}
-                                                                    >
+                                                            return (
+                                                                <OverlayTrigger
+                                                                    key={index}
+                                                                    placement="top"
+                                                                    overlay={<Tooltip id={`tooltip-${index}`}>Odjazd
+                                                                        o {formattedTime}</Tooltip>}
+                                                                >
                                     <span
                                         className={pulseClass}
                                         style={{
@@ -208,23 +209,23 @@ const TramSchedule: React.FC = () => {
                                     >
                                       {minuteStr}
                                     </span>
-                                                                    </OverlayTrigger>
-                                                                );
-                                                            })}
-                                                        </Col>
-                                                    </Row>
-                                                </ListGroup.Item>
-                                            ))}
-                                        </ListGroup>
-                                    </Tab>
-                                ))}
-                            </Tabs>
-                        </>
-                    ) : (
-                        <Alert variant="danger">Nie udało się załadować rozkładu jazdy.</Alert>
-                    )}
-                </Card.Body>
-            </Card>
+                                                                </OverlayTrigger>
+                                                            );
+                                                        })}
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+                                        ))}
+                                    </ListGroup>
+                                </Tab>
+                            ))}
+                        </Tabs>
+                    </>
+                ) : (
+                    <Alert variant="danger">Nie udało się załadować rozkładu jazdy.</Alert>
+                )}
+            </Card.Body>
+        </Card>
     );
 };
 
